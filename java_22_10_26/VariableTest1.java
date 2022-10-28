d8090b068b5139228e963b61985b8c316ec00079
Graypigeonhgh <111276643+Graypigeonhgh@users.noreply.github.com>
2022-10-28 14:34:01 +0800

    Add files via upload

---
+++ java_22_10_26/VariableTest1.java
@@ -0,0 +1,30 @@
+class VariableTest1
+
+/*
+Java的数据类型
+*/    
+{
+	public static void main(String[] args) 
+	{
+		int a1=1;
+		int a2=429496729;
+		char b1='a';
+		char b2='黄';
+		char b3='\n';
+		float c1=3.1415926f;
+		double c3=3.1415926535;
+		long c4=3414234324L;
+
+		System.out.println(a1);
+		System.out.println(a2);
+		System.out.println(b1);
+		System.out.println(b2);
+		//System.out.println(b3);
+		System.out.println(c1);
+		System.out.println(c3);
+		System.out.println(c4);
+		System.out.print("Hello World!"+b3);
+		System.out.print("Hello World!");
+
+	}
+}
