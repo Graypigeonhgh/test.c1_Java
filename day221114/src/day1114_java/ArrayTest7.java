3ab727febcfe1af5723fc7303fd8a9ccd5bcfa8d
Graypigeonhgh <111276643+Graypigeonhgh@users.noreply.github.com>
2022-11-15 22:17:58 +0800

    Update ArrayTest7.java

--- day221114/src/day1114_java/ArrayTest7.java
+++ day221114/src/day1114_java/ArrayTest7.java
@@ -22,6 +22,36 @@ public class ArrayTest7
 		{
 			System.out.println("很遗憾，没找到！");
 		}
+		//二分查找（折半查找）所要查找的数组必须有序
+		int[] arr2=new int[] {-98,-67,-34,0,9,10,24,40,78,87};
+		int dest1=scan.nextInt();
+		
+		int head=0;//初始首索引
+		int end=arr2.length-1;//初始末索引
+		boolean isFlag1=true;
+		while(head<=end)
+		{
+			int middle=(head+end)/2;
+			if(dest1==arr2[middle])
+			{
+				System.out.println("找到了，位置为："+middle);
+				isFlag1=false;
+				break;
+			}
+			else if(dest1<arr2[middle])
+			{
+				end=middle-1;
+			}
+			else
+			{
+				head=middle+1;
+			}
+		}
+		if(isFlag1==true)
+		{
+			System.out.println("很遗憾，没找到！");
+		}
+
 		
 	}
 }
