2c13f71860bbc5f05b941d38dec7cb8ee46e6886
Graypigeonhgh <111276643+Graypigeonhgh@users.noreply.github.com>
2022-11-29 21:57:01 +0800

    Update PersonTest.java

--- day221129/PersonTest.java
+++ day221129/PersonTest.java
@@ -6,6 +6,10 @@ package day221129;
  *  @version
  *  @date 2022年11月29日下午9:31:38
  *
+ * 要求:
+ *(1)创建Person类的对象，设置该对象的name、age和sex属性，调用study方法，输出字符串“studying”，
+ *调用showAge()方法显示age值，调用addAge()方法给对象的age属性值增加2岁。
+ *(2)创建第二个对象，执行上述操作，体会同一个类的不同对象之间的关系。
  *创建类的对象并调用对象的结构
  */
 public class PersonTest {
