52392bb4e7c20bf11efad0a732121c93bbc16fb7
Graypigeonhgh <111276643+Graypigeonhgh@users.noreply.github.com>
2022-11-30 23:45:25 +0800

    Add files via upload

---
+++ day221130/CirlcleTest.java
@@ -0,0 +1,33 @@
+package day221130;
+/**
+ * 
+ *  @Description
+ *  @author  greypigeon  Email:2371849349@qq.com
+ *  @version
+ *  @date 2022年11月30日下午9:28:18
+ *
+ */
+public class CirlcleTest {
+	public static void main(String[] args) {
+		Circle c1=new Circle();
+		c1.radius=2;
+		//对应方式一
+		double area=c1.findArea();
+		System.out.println(area);
+	}
+}
+
+
+//创建类
+class Circle{
+	//属性
+	double radius;
+	
+	//求圆面积
+	//方式一：
+	public double findArea() {
+		double area=Math.PI*radius*radius;
+		System.out.println("面积为："+area);
+		return area;
+	}
+}
