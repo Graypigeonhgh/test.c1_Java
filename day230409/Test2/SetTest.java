f2fe0c60992cad43958f3e160927a849f544a04a
Graypigeonhgh <111276643+Graypigeonhgh@users.noreply.github.com>
2023-04-10 22:27:51 +0800

    Update SetTest.java

--- day230409/Test2/SetTest.java
+++ day230409/Test2/SetTest.java
@@ -4,10 +4,11 @@ import org.junit.Test;
 
 import java.util.HashSet;
 import java.util.Iterator;
+import java.util.LinkedHashSet;
 import java.util.Set;
 
 /**
- * @description:    Set的无序性与不可重复性的理解
+ * @description:    Set的无序性与不可重复性的理解；HashSet和LinkedHashSet的使用
  * @authod: GreyPigeon mail:2371849349@qq.com
  * @date: 2023-04-09-15:42
  **/
@@ -16,6 +17,25 @@ public class SetTest {
     @Test
     public void test1(){
         Set set=new HashSet();
+        set.add(456);
+        set.add(123);
+        set.add("AA");
+        set.add("BB");
+        set.add(new User("Tom",20));
+        set.add(new User("Tom",20));
+        set.add(129);
+
+        Iterator iterator=set.iterator();
+        while(iterator.hasNext()){
+            System.out.println(iterator.next());
+        }
+    }
+
+    //LinkedHashSet的使用
+    //LinkedHashSet作为HashSet的子类，在添加数据的同时，每个数据还维护了两个引用，记录此数据前一个数据和后一个数据
+    @Test
+    public void test2(){
+        Set set=new LinkedHashSet();
         set.add(456);
         set.add(123);
         set.add("AA");
