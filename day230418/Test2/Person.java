b1f3f9f899d85b4ae73c4bb55cb369d2b8738041
Graypigeonhgh <111276643+Graypigeonhgh@users.noreply.github.com>
2023-04-19 22:41:24 +0800

    Add files via upload

---
+++ day230418/Test2/Person.java
@@ -0,0 +1,9 @@
+package day230418.Test2;
+
+/**
+ * @description:
+ * @authod: GreyPigeon mail:2371849349@qq.com
+ * @date: 2023-04-19-21:59
+ **/
+public class Person {
+}
---
+++ day230418/Test2/Student1.java
@@ -0,0 +1,9 @@
+package day230418.Test2;
+
+/**
+ * @description:
+ * @authod: GreyPigeon mail:2371849349@qq.com
+ * @date: 2023-04-19-22:02
+ **/
+public class Student1 extends Person{
+}
